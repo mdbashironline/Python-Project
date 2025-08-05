77845776080f54ccf6f701fdc076441ae4afd1a2
mdbashironline <mdbashironline@gmail.com>
2025-08-05 18:05:41 +0530

    Add files via upload

---
+++ For_Loop.py
@@ -0,0 +1,18 @@
+# With for Loop
+
+a = ("Oneplus","Vivo","Redmi","Samsung","Nokia")
+# for i in a:
+#     print(i)
+
+#along with range and length in for loop
+# for i in range(len(a)):
+#     print(a[i])
+
+# along with while loop
+i = 0
+while i < len(a):
+    print(a[i])
+    i+=1
+    
+
+
