c5599d37de98b6a0260863ef3a11fa451e0d26ed
mdbashironline <mdbashironline@gmail.com>
2025-08-11 17:32:44 +0530

    A.file with index and refer
    New code has been written in the index.html and refer.py

---
+++ refer.py
@@ -0,0 +1,9 @@
+# create a function to add two numbers
+def add_numbers(a, b):
+    """Returns the sum of two numbers."""
+    return a + b    
+
+# create a function to subtract two numbers
+def subtract_numbers(a, b):
+    """Returns the difference of two numbers."""
+    return a - b
